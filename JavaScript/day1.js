e7bc121a1f5bb5e52f88ed7271f9a1a35a1c71a6
prsndp <prasandeepp@gmail.com>
2022-09-09 22:31:19 +0530

    js day1

---
+++ JavaScript/day1.js
@@ -0,0 +1,141 @@
+// // data types and variables
+
+// /*
+// DATA TYPES:
+
+// undefined
+// null
+// boolean
+// string
+// symbol
+// number
+// object
+
+// */
+// // var a;
+// // var b = 2;
+// // console.log(a);
+// // a = 7;
+// // b = a;
+// // console.log(a);
+
+// var a;
+// var b;
+// var c;
+
+
+// // Do not change code below this line
+
+// a = a+1;
+// b = b+5;
+// c = c+" String! ";
+// console.log(c);
+
+// // Js is casesensative
+// // adding number
+
+// var num = 10+10;
+
+// console.log(num)
+// // substracting number
+
+// var difference  = 45-33;
+// console.log(difference)
+// // product
+
+// var product= 8*10;
+// console.log(product);
+
+// //dividing number
+// var quotient = 66/33;
+// console.log(quotient);
+// //Increament Number 
+// var myVar = 87;
+
+// console.log(++myVar);
+
+// // decimal number 
+
+// var ourDecimal = 5.7;
+// var myDecimal = 0.009;
+// // multiply decimal
+
+// var product  = 2.0*0.0;
+// console.log(product);
+
+// // devide 
+
+// var quotient  = 1.0/2.2;
+// console.log(quotient);
+
+
+// // remainder
+// console.log(11%3);
+// // only modify code below this line
+
+// a = a*5;
+// b = 3*b;
+// console.log(b);
+
+var myStr = '<a href="https://www.example.com" targe="_blank>'
+console.log(myStr);
+
+
+// \' single qoute'
+// \" double qoute
+// \\ backslash
+// \n newline
+// \r carriage return
+// \t tab space 
+// \b backspace 
+// \f form feed
+
+// concanating string
+
+// var ourStr = "I come first. "+"I come second.";
+
+// // Only change code below this line
+
+// var myStr = "This is the start."+"This is new string";
+// console.log(myStr);
+
+
+// var firstnameone = 0;
+// var firstname = "Ada";
+
+// firstnameone - firstname.length;
+
+// var lastname = 0;
+//  lastname = "Lovelace";
+
+// // only change code below this line
+
+// lastname = lastname.length;
+// console.log(lastname);
+
+
+// bracket notation
+
+// var firstletterOfFirstname = "";
+// var firstname = "Ada";
+
+// firstLetterOffFirstname  = firstname[0];
+
+// // setup
+// var firstLetteroflastName = "";
+// var lastname = "lovelace";
+
+// firstLetteroflastName = lastname[0];
+// console.log(firstLetteroflastName)
+
+
+// var myStr = "Jello World";
+// myStr = "hello World!";
+// console.log(myStr);
+
+
+// Bracket Notations
+
+var firstname = "Ada";
+var thirdname = firstname[firstname.length-2];
+console.log(firstname);
